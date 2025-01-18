9f8316ef4f838fbe020a83c1abf01f6724b9616b
shreyas kapse <shreyaskapse1234@gmail.com>
2025-01-18 20:23:47 +0530

    Dashboard created
    Add dashboard layout and user button component; implement utility functions and update global styles

--- app/page.js
+++ app/page.js
@@ -1,9 +1,11 @@
 import Image from "next/image";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
     <div>
       <h1>Home</h1>
+      <Button className= "hover:scale-105 hover:shadow-lg">Click Me</Button>
     </div>
   );
 }
